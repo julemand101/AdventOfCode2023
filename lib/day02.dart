726b15c6f748fbdccebadc7ea1b12b9ec037f425
Jacob Bang <julemand101@archlinux.dk>
2023-12-02 13:10:04 +0100

    Day02: Solved and improved

---
+++ lib/day02.dart
@@ -0,0 +1,52 @@
+// --- Day 2: Cube Conundrum ---
+// https://adventofcode.com/2023/day/2
+
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+
+int solveA(Iterable<String> input) => input
+    .map(parse)
+    .where((game) => game.sets
+        .every((set) => set.red <= 12 && set.green <= 13 && set.blue <= 14))
+    .fold(0, (sum, game) => sum + game.id);
+
+int solveB(Iterable<String> input) => input
+    .map(parse)
+    .map((game) => game.sets.reduce((g1, g2) => (
+          red: max(g1.red, g2.red),
+          green: max(g1.green, g2.green),
+          blue: max(g1.blue, g2.blue)
+        )))
+    .map((e) => e.red * e.green * e.blue)
+    .sum;
+
+final regExp = RegExp(r'(\d+) (red|green|blue)');
+
+({int id, Iterable<({int red, int green, int blue})> sets}) parse(String line) {
+  final [gameIdString, rest] = line.substring('Game '.length).split(': ');
+
+  return (
+    id: int.parse(gameIdString),
+    sets: rest.split('; ').map((e) => regExp.allMatches(e)).map((matches) {
+      var red = 0, green = 0, blue = 0;
+
+      for (final match in matches) {
+        final amount = int.parse(match[1]!);
+
+        switch (match[2]) {
+          case 'red':
+            red = amount;
+          case 'green':
+            green = amount;
+          case 'blue':
+            blue = amount;
+          default:
+            throw Exception('${match[2]} is not a know color!');
+        }
+      }
+
+      return (red: red, green: green, blue: blue);
+    })
+  );
+}
---
+++ test/day02_test.dart
@@ -0,0 +1,46 @@
+// --- Day 2: Cube Conundrum ---
+// https://adventofcode.com/2023/day/2
+
+import 'dart:io';
+import 'package:advent_of_code_2023/day02.dart';
+import 'package:advent_of_code_2023/util.dart';
+import 'package:test/test.dart';
+
+final input = File('test/data/day02.txt').readAsLinesSync();
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(
+          solveA('''
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green          
+'''
+              .asLines),
+          equals(8));
+    });
+    test('Solution', () {
+      expect(solveA(input), equals(1867));
+    });
+  });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(
+          solveB('''
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green       
+'''
+              .asLines),
+          equals(2286));
+    });
+    test('Solution', () {
+      expect(solveB(input), equals(84538));
+    });
+  });
+}
