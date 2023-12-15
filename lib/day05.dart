14a79eed29831ef29072df8e3bc28c3ea238b45c
Jacob Bang <julemand101@archlinux.dk>
2023-12-15 21:13:50 +0100

    Day05a: Solved
    Also tried to solve second part but failed.
    
    Need to implement some optimizations. :)

---
+++ lib/day05.dart
@@ -0,0 +1,110 @@
+// --- Day 5: If You Give A Seed A Fertilizer ---
+// https://adventofcode.com/2023/day/5
+
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+
+final regExp = RegExp('(.*)-to-(.*) map:');
+
+int solveA(List<String> input) {
+  final (:seeds, :lookup) = parse(input);
+  return seeds.map(lookup['seed']!.convert).reduce(min);
+}
+
+int solveB(List<String> input) {
+  final (:seeds, :lookup) = parse(input);
+
+  return seeds
+      .slices(2)
+      .expand((list) => Iterable.generate(list[1], (index) => index + list[0]))
+      .map(lookup['seed']!.convert)
+      .reduce(min);
+}
+
+({List<int> seeds, Map<String, AlmanacMap> lookup}) parse(List<String> input) {
+  final seeds = [...input.first.split(' ').skip(1).map(int.parse)];
+  final lookup = <String, AlmanacMap>{};
+
+  for (final lines in input
+      .skip(2)
+      .where((line) => line.isNotEmpty)
+      .splitBefore((line) => line.contains('map:'))) {
+    final match = regExp.firstMatch(lines.first)!;
+
+    final almanacMap = AlmanacMap(
+      form: match[1]!,
+      to: match[2]!,
+      intervals: [...lines.skip(1).map(Interval.parse)],
+    );
+    lookup[almanacMap.form] = almanacMap;
+  }
+
+  for (final almanacMap in lookup.values) {
+    almanacMap.next = lookup[almanacMap.to];
+  }
+
+  return (seeds: seeds, lookup: lookup);
+}
+
+class Interval {
+  final int destinationCategory;
+  final int sourceCategory;
+  final int rangeLength;
+
+  const Interval({
+    required this.destinationCategory,
+    required this.sourceCategory,
+    required this.rangeLength,
+  });
+
+  factory Interval.parse(String line) => switch (line.split(' ')) {
+        [
+          final String destinationCategory,
+          final String sourceCategory,
+          final String rangeLength
+        ] =>
+          Interval(
+              destinationCategory: int.parse(destinationCategory),
+              sourceCategory: int.parse(sourceCategory),
+              rangeLength: int.parse(rangeLength)),
+        final list => throw Exception('Could not handle: $list'),
+      };
+
+  bool inRange(int sourceValue) =>
+      sourceValue >= sourceCategory &&
+      sourceValue <= sourceCategory + rangeLength;
+
+  int convert(int sourceValue) => inRange(sourceValue)
+      ? (sourceValue - sourceCategory) + destinationCategory
+      : sourceValue;
+}
+
+class AlmanacMap {
+  final String form;
+  final String to;
+  final List<Interval> intervals;
+  AlmanacMap? next;
+
+  AlmanacMap({
+    required this.form,
+    required this.to,
+    required this.intervals,
+  });
+
+  int convert(int inputValue) {
+    AlmanacMap? currentAlmanacMap = this;
+    var value = inputValue;
+
+    while (currentAlmanacMap != null) {
+      if (currentAlmanacMap.intervals
+              .firstWhereOrNull((interval) => interval.inRange(value))
+          case final interval?) {
+        value = interval.convert(value);
+      }
+      currentAlmanacMap = currentAlmanacMap.next;
+    }
+
+    return value;
+  }
+}
---
+++ test/day05_test.dart
@@ -0,0 +1,104 @@
+// --- Day 5: If You Give A Seed A Fertilizer ---
+// https://adventofcode.com/2023/day/5
+
+import 'dart:io';
+import 'package:advent_of_code_2023/day05.dart';
+import 'package:advent_of_code_2023/util.dart';
+import 'package:test/test.dart';
+
+final input = File('test/data/day05.txt').readAsLinesSync();
+
+void main() {
+  group('Part One', () {
+    test('Example 1', () {
+      expect(
+          solveA(r'''
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+'''
+              .asLines
+              .toList(growable: false)),
+          equals(35));
+    });
+    test('Solution', () {
+      expect(solveA(input), equals(510109797));
+    });
+  });
+  group('Part Two', () {
+    test('Example 1', () {
+      expect(
+          solveB(r'''
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+'''
+              .asLines
+              .toList(growable: false)),
+          equals(46));
+    });
+    test('Solution', () {
+      expect(solveB(input), equals(-1));
+    }, skip: true);
+  });
+}
